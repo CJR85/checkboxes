276b559b99fee90f91a86e671910189b8eb925fd
Calvin Roberts <roberts.calvin85@gmail.com>
2021-03-13 23:07:15 +0000

    Javascript Logic.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-'use strict';
+const toggles = document.querySelectorAll('.toggle'),
+  good = document.querySelector('#good'),
+  cheap = document.querySelector('#cheap'),
+  fast = document.querySelector('#fast');
+
+toggles.forEach((toggle) =>
+  toggle.addEventListener('change', (e) => doTheTrick(e.target))
+);
+
+function doTheTrick(theClickedOne) {
+  if (good.checked && cheap.checked && fast.checked) {
+    if (good === theClickedOne) {
+      fast.checked = false;
+    }
+
+    if (cheap === theClickedOne) {
+      good.checked = false;
+    }
+
+    if (fast === theClickedOne) {
+      cheap.checked = false;
+    }
+  }
+}
